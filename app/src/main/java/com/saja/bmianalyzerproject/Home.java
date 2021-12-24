212476376b02467054faf3289fa94b1fd8f689f6
SajaBallour <abdsaja3@gmail.com>
2021-12-24 02:48:31 +0200

    update

--- app/src/main/java/com/saja/bmianalyzerproject/Home.java
+++ app/src/main/java/com/saja/bmianalyzerproject/Home.java
@@ -4,16 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Home extends AppCompatActivity {
     Timer t;
+    TextView logout;
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        logout = findViewById(R.id.Logout);
+        mAuth = FirebaseAuth.getInstance();
+        logout.setOnClickListener( view -> {
+            mAuth.signOut();
+            startActivity(new Intent(Home.this,Login.class));
+
+        });
+
         /*t = new Timer();
         t.schedule(new TimerTask() {
             @Override
--- app/src/main/java/com/saja/bmianalyzerproject/Login.java
+++ app/src/main/java/com/saja/bmianalyzerproject/Login.java
@@ -1,58 +1,97 @@
 package com.saja.bmianalyzerproject;
 
-import static android.text.TextUtils.isEmpty;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-//import android.content.Intent;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
-import java.util.Timer;
-//import java.util.TimerTask;
-import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Timer;
 
 public class Login extends AppCompatActivity {
-    EditText username;
-    EditText password;
-    Button login;
-    TextView signup;
+    EditText Username;
+    EditText Password;
+    Button Login;
+    TextView Signup;
     FirebaseAuth mAuth;
-    //Timer t;
+    Timer t;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        username = findViewById(R.id.username);
-        password = findViewById(R.id.password);
-        login = findViewById(R.id.Button_Login);
-        signup = findViewById(R.id.signup);
+        Username=findViewById(R.id.username);
+        Password =findViewById(R.id.password);
+        Login =findViewById(R.id.Button_Login);
+        Signup =findViewById(R.id.signup);
         mAuth = FirebaseAuth.getInstance();
-
-     /*   signup.setOnClickListener(v -> {
+        Login.setOnClickListener(view ->{
+            login();
+        });
+        Signup.setOnClickListener(view -> {
 
             startActivity(new Intent(Login.this,Registration.class));
-        });*/
-        login.setOnClickListener((View.OnClickListener) view -> login(username.getText().toString(), password.getText().toString()));
-        signup.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(),Registration.class)));
-
-
-
+        });
 
     }
-    private void login(String userName, String userPassword) {
+
+    private void login() {
+        String username =  Username.getText().toString().trim();
+        String password = Password.getText().toString().trim();
+
+        if (TextUtils.isEmpty(username)){
+            Username.setError("You must enter Username");
+           Username.requestFocus();
+        }else if(TextUtils.isEmpty(password)){
+            Password.setError("You must enter Password");
+            Password.requestFocus();
+        }else {
+
+            mAuth.signInWithEmailAndPassword(username,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        checkEmail();
+                    }else{
+                        Toast.makeText(Login.this, "Login Error: "+task.getException().getLocalizedMessage() + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+
+                    }
+
+                }
+            });
+
+        }
+
+    }
+    private void checkEmail(){
+        FirebaseUser users=FirebaseAuth.getInstance().getCurrentUser();
+        if(users.isEmailVerified()){
+            Toast.makeText(this,"Verify the Email Id",Toast.LENGTH_SHORT).show();
+            mAuth.signOut();
+            finish();
+        }
+        else {
+            Username.getText().clear();
+            Password.getText().clear();
+            Intent intent = new Intent(Login.this, Home.class);
+            startActivity(intent);
+        }
+    }
+
+
+    /*private void login(String userName, String userPassword) {
         if(username.getText().toString().equals("saja") && password.getText().toString().equals("saja")){
             Intent intent = new Intent(Login.this, Home.class);
             startActivity(intent);
@@ -63,7 +102,7 @@ public class Login extends AppCompatActivity {
             if (isEmpty(userPassword))
                 password.setError("You must enter Password to login!");
         }
-    }
+    }*/
     /*
     t = new Timer();
         t.schedule(new TimerTask() {
